9e703dede5996c2c1010e3d32e970ce9037101f4
Georgy Nikishin <nikishin.gs@phystech.edu>
2021-05-21 00:11:28 +0300

    commit

--- coursera_house/core/views.py
+++ coursera_house/core/views.py
@@ -2,16 +2,7 @@ import requests
 from django.conf import settings
 from django.urls import reverse_lazy
 from django.views.generic import FormView
-from django.urls import reverse_lazy
-from django.views.generic import FormView
-from django.http import HttpResponse, HttpResponseRedirect
-from django.conf import settings
-from marshmallow import Schema, fields
-from marshmallow.validate import Range
-from marshmallow import ValidationError
-from .models import Setting
-from .form import ControllerForm
-from .tasks import smart_home_manager
+
 from .models import Setting
 from .form import ControllerForm
 
@@ -20,16 +11,18 @@ url = settings.SMART_HOME_API_URL
 headers = {'Authorization': f'Bearer {TOKEN}'}
 
 
-class MyBoolean(fields.Boolean):
-    truthy = fields.Boolean.truthy.union({"on"})
-    falsy = fields.Boolean.falsy.union({"off"})
-
-
-class ControllerViewSchema(Schema):
-    bedroom_target_temperature = fields.Int(validate=Range(16, 50), default=21)
-    hot_water_target_temperature = fields.Int(validate=Range(24, 90), default=80)
-    bedroom_light = MyBoolean()
-    bathroom_light = MyBoolean()
+def get_or_update(controller_name, label, value):
+    try:
+        entry = Setting.objects.get(controller_name=controller_name)
+    except Setting.DoesNotExist:
+        Setting.objects.create(
+            controller_name=controller_name,
+            label=label,
+            value=value
+        )
+    else:
+        entry.value = value
+        entry.save()
 
 
 class ControllerView(FormView):
@@ -37,73 +30,62 @@ class ControllerView(FormView):
     template_name = 'core/control.html'
     success_url = reverse_lazy('form')
 
+    def get(self, request, *args, **kwargs):
+        context = self.get_context_data()
+        if not context.get('data'):
+            return self.render_to_response(context, status=502)
+        return self.render_to_response(context)
+
+
     def get_context_data(self, **kwargs):
         context = super(ControllerView, self).get_context_data()
+        headers = {'Authorization': f'Bearer {settings.SMART_HOME_ACCESS_TOKEN}'}
         try:
-            context['data'] = settings.TEMPLATE_DATA['controllers']
-            # print(context['data'])
-        except ValueError:
-            print('Decoding JSON has failed')
+            current_controller_data = requests.get(
+                settings.SMART_HOME_API_URL, headers=headers
+            ).json()
+            context['data'] = current_controller_data
+        except:
+            context['data'] = {}
         return context
 
     def get_initial(self):
-
-        params = {
-            'bedroom_target_temperature': 21,
-            'hot_water_target_temperature': 80,
-            'bedroom_light': False,
-            'bathroom_light': False
-        }
-        if Setting.objects.get(controller_name='bedroom_target_temperature'):
-            params['bedroom_target_temperature'] = Setting.objects.get(
-                controller_name='bedroom_target_temperature').value
-        if Setting.objects.get(controller_name='hot_water_target_temperature'):
-            params['hot_water_target_temperature'] = Setting.objects.get(
-                controller_name='hot_water_target_temperature').value
-        smart_home_manager()
-        return params
+        initial = super(ControllerView, self).get_initial()
+        initial['bedroom_target_temperature'] = 21
+        initial['hot_water_target_temperature'] = 80
+        return initial
 
     def form_valid(self, form):
-        try:
-            schema = ControllerViewSchema()
-            data = schema.load(self.request.POST)
-            bedroom_target_temperature = data.data['bedroom_target_temperature']
-            hot_water_target_temperature = data.data['hot_water_target_temperature']
-            bedroom_light = False
-            bathroom_light = False
-            if "bedroom_light" in data.data:
-                bedroom_light = data.data["bedroom_light"]
+        get_or_update(
+            'bedroom_target_temperature',
+            'Bedroom target temperature',
+            form.cleaned_data['bedroom_target_temperature']
+        )
+        get_or_update(
+            'hot_water_target_temperature',
+            'Hot water target temperature value',
+            form.cleaned_data['hot_water_target_temperature']
+        )
+        controller_data = requests.get(url, headers=headers).json().get('data')
+        if controller_data:
+            controller_bedroom_light = list(
+                filter(lambda x: 'bedroom_light' in x.values(), controller_data)
+            )[0]['value']
+            controller_bathroom_light = list(
+                filter(lambda x: 'bathroom_light' in x.values(), controller_data)
+            )[0]['value']
+            smoke_detector = list(
+                filter(lambda x: 'smoke_detector' in x.values(),
+                       controller_data)
+            )[0]['value']
+            payload = {'controllers': []}
+            if not smoke_detector:
+                if form.cleaned_data['bedroom_light'] != controller_bedroom_light:
+                    payload['controllers'].append(
+                        {'name': 'bedroom_light', 'value': form.cleaned_data['bedroom_light']})
+                if form.cleaned_data['bathroom_light'] != controller_bathroom_light:
+                    payload['controllers'].append(
+                        {'name': 'bathroom_light', 'value': form.cleaned_data['bathroom_light']})
+                requests.post(url, headers=headers, json=payload)
 
-            if "bathroom_light" in data.data:
-                bathroom_light = data.data["bathroom_light"]
-
-            if Setting.objects.get(controller_name='bedroom_target_temperature'):
-                bedroom_target_temperature_model = Setting.objects.get(controller_name='bedroom_target_temperature')
-                bedroom_target_temperature_model.value = bedroom_target_temperature
-                bedroom_target_temperature_model.save()
-            else:
-                bedroom_target_temperature_model = Setting(
-                    controller_name='bedroom_target_temperature',
-                    label='bedroom_target_temperature',
-                    value=bedroom_target_temperature
-                )
-                bedroom_target_temperature_model.save()
-
-            if Setting.objects.get(controller_name='hot_water_target_temperature'):
-                hot_water_target_temperature_model = Setting.objects.get(controller_name='hot_water_target_temperature')
-                hot_water_target_temperature_model.value = hot_water_target_temperature
-                hot_water_target_temperature_model.save()
-            else:
-                hot_water_target_temperature_model = Setting(
-                    controller_name='hot_water_target_temperature',
-                    label='hot_water_target_temperature',
-                    value=hot_water_target_temperature
-                )
-                hot_water_target_temperature_model.save()
-
-        except ValidationError as exc:
-            return HttpResponseRedirect('/')
-        except ValueError:
-            print('Decoding JSON has failed')
-
-        return super(ControllerView, self).form_valid(form)
+        return super(ControllerView, self).get(form)
