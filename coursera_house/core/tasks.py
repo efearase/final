43988cb646e82c387f9baece5ac24ca18e4ba1c4
Georgy Nikishin <nikishin.gs@phystech.edu>
2021-05-20 19:54:16 +0300

    commit

--- coursera_house/core/tasks.py
+++ coursera_house/core/tasks.py
@@ -1,266 +1,116 @@
 from __future__ import absolute_import, unicode_literals
-
-from json.decoder import JSONDecodeError
-
-from celery import task
 import requests
-from django.core.mail import send_mail
-
+from celery import task
 from django.conf import settings
+from django.core.mail import EmailMessage
 from .models import Setting
+
+
+TOKEN = settings.SMART_HOME_ACCESS_TOKEN
+url = settings.SMART_HOME_API_URL
+headers = {'Authorization': f'Bearer {TOKEN}'}
 
 
 @task()
 def smart_home_manager():
-    try:
-        data = get_data()
-        settings.TEMPLATE_DATA = data
-        if data != 0:
-            new_data = check_and_update_data(data)
-            if new_data["controllers"]:
-                post_data(new_data)
-        else:
-            raise ValueError
-    except ValueError:
-        print('Decoding JSON has failed')
-
-
-def get_data():
-    controller_url = settings.SMART_HOME_API_URL
-    final_data = {
-        "controllers": []
+    controller_data = requests.get(url, headers=headers).json().get('data')
+    controller_data = {x['name']: x for x in controller_data}
+    payload = {
+        'controllers': []
     }
-    try:
-        data = requests.get(
-            controller_url,
-            headers={'Authorization': 'Bearer {}'.format(settings.SMART_HOME_ACCESS_TOKEN)}
+
+    if controller_data['leak_detector']['value']:
+        # если датчик показывает протечку и есть гор. и/или хол. вода,
+        # перекрываем гор. и/или хол. воду
+        if controller_data['cold_water']['value']:
+            payload['controllers'].append({'name': 'cold_water', 'value': False})
+
+        if controller_data['hot_water']['value']:
+            payload['controllers'].append({'name': 'hot_water', 'value': False})
+        email = EmailMessage(
+            'leak detector',
+            'text',
+            settings.EMAIL_HOST,
+            [settings.EMAIL_RECEPIENT],
         )
+        email.send(fail_silently=False)
 
-        if data.status_code != requests.codes.ok:
-            return 0
-        else:
-            data = data.json()
+    # если протечка или нет холодной воды
+    if controller_data['leak_detector']['value'] or \
+            not controller_data['cold_water']['value']:
+        if controller_data['boiler']['value']:
+            payload['controllers'].append({'name': 'boiler', 'value': False})
+        if controller_data['washing_machine']['value'] in ('on', 'broken'):
+            payload['controllers'].append({'name': 'washing_machine', 'value': "off"})
 
-        data = data['data']
+    boiler_temperature = controller_data['boiler_temperature']['value']
+    hot_water_target_temperature = Setting.objects.get(
+        controller_name='hot_water_target_temperature').value
 
-        for item in data:
-            final_data['controllers'].append(
-                {
-                    "name": item['name'],
-                    "value": item['value']
-                }
+    bedroom_temperature = controller_data['bedroom_temperature']['value']
+    bedroom_target_temperature = Setting.objects.get(
+        controller_name='bedroom_target_temperature').value
+
+    if controller_data['smoke_detector']['value']:
+        # если дым, выключаем кондиционер, бойлер и свет, но только если они включены
+        if controller_data['air_conditioner']['value']:
+            payload['controllers'].append(
+                {'name': 'air_conditioner', 'value': False}
+            )
+        if controller_data['bathroom_light']['value']:
+            payload['controllers'].append(
+                {'name': 'bathroom_light', 'value': False}
+            )
+        if controller_data['bedroom_light']['value']:
+            payload['controllers'].append(
+                {'name': 'bedroom_light', 'value': False}
+            )
+        if controller_data['boiler']['value']:
+            payload['controllers'].append({'name': 'boiler', 'value': False})
+        if controller_data['washing_machine']['value'] in ('on', 'broken'):
+            payload['controllers'].append(
+                {'name': 'washing_machine', 'value': 'off'}
             )
 
-    except ValueError:
-        print('Decoding JSON has failed')
-
-    return final_data
-
-
-def post_data(final_data):
-    controller_url = settings.SMART_HOME_API_URL
-    try:
-        requests.post(
-            controller_url,
-            headers={'Authorization': 'Bearer {}'.format(settings.SMART_HOME_ACCESS_TOKEN)},
-            json=final_data
-        )
-    except ValueError:
-        print('Decoding JSON has failed')
-
-
-def check_and_update_data(data):
-    final_data = {
-        "controllers": []
+    can_turn_on = {
+        'boiler': controller_data['cold_water']['value'] and \
+                  not controller_data['leak_detector']['value'] and \
+                  not controller_data['smoke_detector']['value'] and \
+                  not controller_data['boiler']
+        ,
+        'air_conditioner': not controller_data['smoke_detector']['value']
     }
-    leak_detector = False
-    cold_water = True
-    try:
-        for item in data['controllers']:
-            # 1
-            if item['name'] == 'leak_detector' and item['value'] is True:
-                leak_detector = True
-                ch_send_mail()
-                final_data["controllers"].append(
-                    {
-                        "name": 'cold_water',
-                        "value": False
-                    }
-                )
-                final_data["controllers"].append(
-                    {
-                        "name": 'hot_water',
-                        "value": False
-                    }
-                )
-        for item in data['controllers']:
-            # 2
-            if item['name'] == 'cold_water' and (item['value'] is False or leak_detector is True):
-                cold_water = False
-                for item2 in data['controllers']:
-                    if item2['name'] == 'boiler' and item2['value'] is True:
-                        final_data["controllers"].append(
-                            {
-                                "name": 'boiler',
-                                "value": False
-                            }
-                        )
-                for item2 in data['controllers']:
-                    if item2['name'] == 'washing_machine' and item2['value'] != 'off':
-                        final_data["controllers"].append(
-                            {
-                                "name": 'washing_machine',
-                                "value": 'off'
-                            }
-                        )
-            elif item['name'] == 'cold_water' and item['value'] is True and leak_detector is False:
-                for item2 in data['controllers']:
-                    # 3
-                    if item2['name'] == 'boiler_temperature':
-                        boiler_temperature = item2['value']
-                        hot_water_target_temperature = \
-                            Setting.objects.get(controller_name='hot_water_target_temperature').value
-                        if boiler_temperature < hot_water_target_temperature * 0.9:
-                            for item3 in data['controllers']:
-                                if item3['name'] == 'smoke_detector' and item3['value'] is not True:
-                                    for item4 in data['controllers']:
-                                        if item4['name'] == 'boiler' and item4['value'] is not True:
-                                            final_data["controllers"].append(
-                                                {
-                                                    "name": 'boiler',
-                                                    "value": True
-                                                }
-                                            )
-                        if boiler_temperature >= hot_water_target_temperature * 1.1:
-                            for item4 in data['controllers']:
-                                if item4['name'] == 'boiler' and item4['value'] is not False:
-                                    final_data["controllers"].append(
-                                        {
-                                            "name": 'boiler',
-                                            "value": False
-                                        }
-                                    )
-        for item in data['controllers']:
-            # 4,5
-            if item['name'] == 'outdoor_light' and item['value'] > 50:
-                for item2 in data['controllers']:
-                    if item2['name'] == 'bedroom_light' and item2['value'] is False:
-                        for item3 in data['controllers']:
-                            if item3['name'] == 'curtains' and item3['value'] != 'slightly_open':
-                                for item4 in data['controllers']:
-                                    if item4['name'] == 'curtains' and item4['value'] != 'open':
-                                        final_data["controllers"].append(
-                                            {
-                                                "name": 'curtains',
-                                                "value": 'open'
-                                            }
-                                        )
 
-            elif (item['name'] == 'outdoor_light' and item['value'] < 20) or \
-                    (item['name'] == 'bedroom_light' and item['value'] is True):
-                for item2 in data['controllers']:
-                    if item2['name'] == 'curtains' and item2['value'] != 'slightly_open':
-                        for item3 in data['controllers']:
-                            if item3['name'] == 'curtains' and item3['value'] != 'close':
-                                for item4 in final_data['controllers']:
-                                    if item4['name'] == 'curtains':
-                                        break
-                                else:
-                                    final_data["controllers"].append(
-                                        {
-                                            "name": 'curtains',
-                                            "value": 'close'
-                                        }
-                                    )
+    if (boiler_temperature < hot_water_target_temperature * 0.9) and \
+            can_turn_on['boiler']:
+        payload['controllers'].append({'name': 'boiler', 'value': True})
 
-        for item in data['controllers']:
-            # 6
-            if item['name'] == 'smoke_detector' and item['value'] == True:
-                for item2 in data['controllers']:
-                    if item2['name'] == 'air_conditioner' and item2['value'] is not False:
-                        final_data["controllers"].append(
-                            {
-                                "name": 'air_conditioner',
-                                "value": False
-                            }
-                        )
-                for item2 in data['controllers']:
-                    if item2['name'] == 'bedroom_light' and item2['value'] == True:
-                        final_data["controllers"].append(
-                            {
-                                "name": 'bedroom_light',
-                                "value": False
-                            }
-                        )
-                for item2 in data['controllers']:
-                    if item2['name'] == 'bathroom_light' and item2['value'] == True:
-                        final_data["controllers"].append(
-                            {
-                                "name": 'bathroom_light',
-                                "value": False
-                            }
-                        )
-                for item3 in final_data['controllers']:
-                    if item3['name'] == 'boiler':
-                        if item3['value'] is True:
-                            item3['value'] = False
-                        break
-                else:
-                    for item4 in data['controllers']:
-                        if item4['name'] == 'boiler' and item4['value'] == True:
-                            final_data["controllers"].append(
-                                {
-                                    "name": 'boiler',
-                                    "value": False
-                                }
-                            )
-                for item3 in final_data['controllers']:
-                    if item3['name'] == 'washing_machine':
-                        break
-                else:
-                    for item4 in data['controllers']:
-                        if item4['name'] == 'washing_machine' and item4['value'] != 'off':
-                            final_data["controllers"].append(
-                                {
-                                    "name": 'washing_machine',
-                                    "value": 'off'
-                                }
-                            )
-            elif item['name'] == 'smoke_detector' and item['value'] is False:
-                for item1 in data['controllers']:
-                    # 7
-                    if item1['name'] == 'bedroom_temperature':
-                        bedroom_temperature = item1['value']
-                        bedroom_target_temperature = \
-                            Setting.objects.get(controller_name='bedroom_target_temperature').value
-                        if bedroom_temperature > bedroom_target_temperature * 1.1:
-                            for item4 in data['controllers']:
-                                if item4['name'] == 'air_conditioner' and item4['value'] is not True:
-                                    final_data["controllers"].append(
-                                        {
-                                            "name": 'air_conditioner',
-                                            "value": True
-                                        }
-                                    )
-                        if bedroom_temperature <= bedroom_target_temperature * 0.98:
-                            for item4 in data['controllers']:
-                                if item4['name'] == 'air_conditioner' and item4['value'] == True:
-                                    final_data["controllers"].append(
-                                        {
-                                            "name": 'air_conditioner',
-                                            "value": False
-                                        }
-                                    )
-    except ValueError:
-        print('Decoding JSON has failed')
-    return final_data
+    if boiler_temperature > hot_water_target_temperature * 1.1:
+        payload['controllers'].append({'name': 'boiler', 'value': False})
+
+    if (bedroom_temperature < bedroom_target_temperature * 0.9) and \
+            can_turn_on['air_conditioner']:
+        payload['controllers'].append({'name': 'air_conditioner', 'value': False})
+
+    if bedroom_temperature > bedroom_target_temperature * 1.1:
+        payload['controllers'].append({'name': 'air_conditioner', 'value': True})
 
 
-def ch_send_mail():
-    send_mail(
-        'умный дом',
-        'вода отключена',
-        'nikishin.gs@phystech.edu',
-        [settings.EMAIL_RECEPIENT],
-        fail_silently=True,
-    )
+
+    outdoor_light = controller_data['outdoor_light']['value']
+    bedroom_light = controller_data['bedroom_light']['value']
+    if controller_data['curtains']['value'] == 'slightly_open':
+        pass
+    else:
+        if outdoor_light < 50 and not bedroom_light:
+            payload['controllers'].append({'name': 'curtains', 'value': 'open'})
+        elif outdoor_light > 50 or bedroom_light:
+            payload['controllers'].append({'name': 'curtains', 'value': 'close'})
+
+    if payload['controllers']:
+        unique = []
+        for item in payload['controllers']:
+            if item not in unique:
+                unique.append(item)
+        payload['controllers'] = unique
+        requests.post(url, headers=headers, json=payload)
